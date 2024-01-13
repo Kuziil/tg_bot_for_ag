bf73658bb8ad51d4f29c41447f35310f9a8c6c86
Kuziil <lfy050@yandex.ru>
2024-01-14 06:35:19 +0700

    новый роутер для user_handlers
    посдоеденил роутер для основных хэндлеров
     Changes to be committed:
    	modified:   bot.py
    	modified:   handlers/user_handlers.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
-from handlers import other_handlers
+from handlers import other_handlers, user_handlers
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
@@ -32,6 +32,7 @@ async def main():
 
     # TODO: Регистриуем роутеры в диспетчере
     dp.include_router(other_handlers.router)
+    dp.include_router(user_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
