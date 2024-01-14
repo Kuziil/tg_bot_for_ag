4e7ea7174001bc0b015ce937bf7df28bfc18c574
Kuziil <lfy050@yandex.ru>
2024-01-14 07:25:52 +0700

    описания к словарям

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -1,12 +1,15 @@
+# текст который пишет бота
 LEXICON_RU: dict[str, str] = {
     'other': 'простите, я вас не понимаю'
 }
 
+# текст которым реагируют команды
 LEXICON_COMMANDS_RU: dict[str, str] = {
     '/start': 'привет!',
     '/help': 'помогу если пойму как'
 }
 
+# описание команд
 LEXICON_COMMANDS_DESC_RU: dict[str, str] = {
     '/start': 'команда start',
     '/help': 'может помогать, но пока не умеет'
