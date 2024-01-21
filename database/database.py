bcde5d2734ef79db14637f07eddc197b08dba40d
Kuziil <lfy050@yandex.ru>
2024-01-21 23:28:16 +0700

    Изменил символ + переименовал некоторые переменные
     Changes to be committed:
    	modified:   database/database.py
    	modified:   keyboards/schedule/kb_schedule.py

--- database/database.py
+++ database/database.py
@@ -24,24 +24,20 @@ class DBManager:
 
     # Функция для добавления смены пользователю
     # TODO: добавить ответ если пользователь не найден day_t
-    def add_shift(self, user_id: int, shift: str) -> str | None:
+    def add_shift(self, user_id: int, day_call_back_t: str) -> str | None:
         if user_id in self.user_database:
-            if shift not in self.shifts:
+            if day_call_back_t not in self.shifts:
                 user = self.user_database[user_id]
-                self.shifts[shift] = user_id
-                user['shifts'].append(shift)
-                logger.info(
-                    f"Смена {shift} добавлена пользователю с ID {user_id}")
+                self.shifts[day_call_back_t] = user_id
+                user['shifts'].append(day_call_back_t)
                 return user['emoticon']
             else:
-                user_id = self.shifts[shift]
-                logger.info(f"Пользователь с ID {user_id} уже занял смену")
+                user_id = self.shifts[day_call_back_t]
                 return self.user_database[user_id]['emoticon']
         else:
-            logger.info(f"Пользователь с ID {user_id} не найден")
-            return '1'
+            return '‼'
 
-    def get_emot_by_shift(self, shift) -> str:
+    def get_emot_by_day_call_back(self, shift) -> str:
         return self.user_database[self.shifts[shift]]['emoticon']
 
 
@@ -49,10 +45,10 @@ class DBManager:
 db = DBManager()
 
 # Пример использования методов класса
-db.add_user(
-    user_id=db.config.tg_bot.admin_ids[0],
-    username="Mic",
-    emoticon="😏")
+# db.add_user(
+#     user_id=db.config.tg_bot.admin_ids[0],
+#     username="Mic",
+#     emoticon="😏")
 
 db.add_user(
     user_id=db.config.tg_bot.operator_ids[0],
