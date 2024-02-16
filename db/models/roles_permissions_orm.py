a36b3f9f88547ab690bfb3fe69e20c901b5356d5
Kuziil <Sikuz70@gmail.com>
2024-02-16 17:55:55 +0700

    switch data type to big int for roles_permissions

--- db/models/roles_permissions_orm.py
+++ db/models/roles_permissions_orm.py
@@ -4,7 +4,7 @@ from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.models.base import Base
-from db.models.types import intpk
+from db.models.types import intpk, intbigint
 
 if TYPE_CHECKING:
     from db.models.roles_orm import RolesORM
@@ -22,13 +22,13 @@ class RolesPermissionsORM(Base):
     )
 
     id: Mapped[intpk]
-    role_id: Mapped[int] = mapped_column(
+    role_id: Mapped[intbigint] = mapped_column(
         ForeignKey(
             column="roles.id",
             ondelete="CASCADE",
         ),
     )
-    permissions_id: Mapped[int] = mapped_column(
+    permissions_id: Mapped[intbigint] = mapped_column(
         ForeignKey(
             column="permissions.id",
             ondelete="CASCADE",
