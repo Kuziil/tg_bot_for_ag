666f2416e7fccf357533ea81a51cab2cb13f68a9
Kuziil <Sikuz70@gmail.com>
2024-02-17 18:57:23 +0700

    сделал проверку на наличие ид бота в бд

--- __main__.py
+++ __main__.py
@@ -3,16 +3,14 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types.user import User
+
 from handlers import main_handlers, other_handlers
 from keyboards.main_menu import set_main_menu
 from config_data.config_reader import settings
-
 from middlewares import DbSessionMiddleware
-from db.requests import test_connection
+from db.requests import test_connection, check_for_bot_id_in_db
 from db.db_helper import DatabaseHelper
-
-
-
 
 
 # Инициализируем логгер
@@ -24,27 +22,25 @@ logger = logging.getLogger(__name__)
 async def main():
     # Конфигурируем логирование
     logging.basicConfig(
-        level=logging.INFO,
-        format='%(filename)s:%(lineno)d #%(levelname)-8s '
-               '[%(asctime)s] - %(name)s - %(message)s')
+        level=logging.DEBUG,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s "
+        "[%(asctime)s] - %(name)s - %(message)s",
+    )
 
     # Выводим в консоль информацию о начале запуска бота
-    logger.info('Starting bot')
+    logger.info("Starting bot")
 
     # Инициализируем хранилище
     storage = MemoryStorage()
     # echo сыпет в консоль
     # pool_size количество подключений
     # max_overflow дополнительные подключения
-    db_helper = DatabaseHelper(db_url=settings.db_url,
-                               echo=True,
-                               pool_size=5,
-                               max_overflow=10)
-    # async with db_helper.engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
-
-    # async with db_helper.engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.create_all)
+    db_helper = DatabaseHelper(
+        db_url=settings.db_url,
+        echo=False,
+        pool_size=5,
+        max_overflow=10,
+    )
 
     async with db_helper.sessionmaker() as session:
         await test_connection(session)
@@ -52,21 +48,31 @@ async def main():
     # Инициализируем бот и диспетчер
     dp = Dispatcher(storage=storage)
 
-    dp.update.middleware(DbSessionMiddleware(
-        session_pool=db_helper.sessionmaker))
+    dp.update.middleware(
+        DbSessionMiddleware(
+            session_pool=db_helper.sessionmaker,
+        ),
+    )
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(main_handlers.main_router)
     dp.include_router(other_handlers.router)
 
-    bot = Bot(token=settings.bot_token.get_secret_value(),
-              parse_mode='HTML')
-
+    bot = Bot(
+        token=settings.bot_token.get_secret_value(),
+        parse_mode="HTML",
+    )
+    info: User = await bot.get_me()
+    logger.info(info.id)
+    async with db_helper.sessionmaker() as session:
+        i = await check_for_bot_id_in_db(session=session, bot_id=info.id)
+        logger.info(i)
     await set_main_menu(bot)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- db/requests.py
+++ db/requests.py
@@ -1,8 +1,15 @@
-from sqlalchemy import select
+import logging
+
+from sqlalchemy import select, or_
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
-from aiogram.types import Message
+from sqlalchemy.exc import NoResultFound, MultipleResultsFound
 
 from db.models import AgenciesORM, UsersORM
+
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
 
 
 async def get_agency_bot_id(session: AsyncSession, agency_id: int):
@@ -20,6 +27,21 @@ async def add_user(session: AsyncSession, username: str, emoji: str):
     user = UsersORM(username=username, emoji=emoji)
     session.add(user)
     await session.commit()
+
+
+async def check_for_bot_id_in_db(session: AsyncSession, bot_id: int):
+    try:
+        stmt = select(AgenciesORM).where(
+            or_(AgenciesORM.tg_bot == bot_id, AgenciesORM.test_tg_bot == bot_id)
+        )
+        result: Result = await session.execute(stmt)
+        agency: AgenciesORM = result.scalar_one()
+        if agency.tg_bot == bot_id:
+            logger.info("The MAIN bot has been started")
+        logger.info("The TEST bot has been started")
+        return agency.id, agency.title
+    except NoResultFound:
+        raise ValueError("Bot ID not found in the database")
 
 
 async def test_connection(session: AsyncSession):
