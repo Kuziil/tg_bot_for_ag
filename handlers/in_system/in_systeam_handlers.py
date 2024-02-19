b2012925345a859b30dbab6ef3c4a1d488a6b8f4
Kuziil <Sikuz70@gmail.com>
2024-02-19 08:46:49 +0700

    отделил расписание по месяцам

--- handlers/in_system/in_systeam_handlers.py
+++ handlers/in_system/in_systeam_handlers.py
@@ -18,22 +18,37 @@ in_systeam_router.include_router(schedule_router)
     F.data == "schedule",
     StateFilter(default_state),
 )
-async def process_cal(
+async def process_schedule_press(
     callback: CallbackQuery,
     i18n: dict[str, dict[str, str]],
 ):
     """Данный хэндлер отрабатывает на нажатие кнопки расписание ->
-    выдает инлайн клавиатуру с расписанием
+    выдает инлайн клавиатуру с типами расписания
 
     Args:
         callback (CallbackQuery): _description_
     """
     buttons: dict[str, str] = i18n["button"]
     await callback.message.edit_text(
-        text=LEXICON_RU["schedule"],
+        text=i18n["lexicon"]["schedule_type"],
         reply_markup=create_menu_keyboard(
-            buttons["mounth_schedule"],
-            buttons["week_schedule"],
+            "mounth_schedule",
+            "week_schedule",
         ),
+    )
+    await callback.answer()
+
+
+@in_systeam_router.callback_query(
+    F.data == "mounth_schedule",
+    StateFilter(default_state),
+)
+async def process_mounth_schedule_press(
+    callback: CallbackQuery,
+    i18n: dict[str, dict[str, str]],
+):
+    await callback.message.edit_text(
+        text=i18n["lexicon"]["schedule"],
+        reply_markup=create_schedule(),
     )
     await callback.answer()
