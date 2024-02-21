3a809265931d091e36b892883a3bc546fc24c92d
Kuziil <Sikuz70@gmail.com>
2024-02-21 16:51:02 +0700

    убрал расписание по неделям

--- handlers/in_system/in_systeam_handlers.py
+++ handlers/in_system/in_systeam_handlers.py
@@ -18,32 +18,32 @@ in_systeam_router = Router()
 in_systeam_router.include_router(schedule_router)
 
 
+# @in_systeam_router.callback_query(
+#     F.data == "schedule",
+#     StateFilter(default_state),
+# )
+# async def process_schedule_press(
+#     callback: CallbackQuery,
+#     i18n: dict[str, dict[str, str]],
+# ):
+#     """Данный хэндлер отрабатывает на нажатие кнопки расписание ->
+#     выдает инлайн клавиатуру с типами расписания
+
+#     Args:
+#         callback (CallbackQuery): _description_
+#     """
+#     await callback.message.edit_text(
+#         text=i18n["lexicon"]["schedule_type"],
+#         reply_markup=create_menu_keyboard(
+#             "mounth_schedule",
+#             "week_schedule",
+#         ),
+#     )
+#     await callback.answer()
+
+
 @in_systeam_router.callback_query(
     F.data == "schedule",
-    StateFilter(default_state),
-)
-async def process_schedule_press(
-    callback: CallbackQuery,
-    i18n: dict[str, dict[str, str]],
-):
-    """Данный хэндлер отрабатывает на нажатие кнопки расписание ->
-    выдает инлайн клавиатуру с типами расписания
-
-    Args:
-        callback (CallbackQuery): _description_
-    """
-    await callback.message.edit_text(
-        text=i18n["lexicon"]["schedule_type"],
-        reply_markup=create_menu_keyboard(
-            "mounth_schedule",
-            "week_schedule",
-        ),
-    )
-    await callback.answer()
-
-
-@in_systeam_router.callback_query(
-    F.data == "mounth_schedule",
     StateFilter(default_state),
 )
 async def process_mounth_schedule_press(
@@ -57,23 +57,23 @@ async def process_mounth_schedule_press(
     await callback.answer()
 
 
-@in_systeam_router.callback_query(
-    F.data == "week_schedule",
-    StateFilter(default_state),
-)
-async def process_mounth_schedule_press(
-    callback: CallbackQuery,
-    session: AsyncSession,
-    i18n: dict[str, dict[str, str]],
-    deafult_tz: ZoneInfo,
-):
-    await callback.message.edit_text(
-        text=i18n["lexicon"]["week_schedule"],
-        reply_markup=await create_week_shudle(
-            session=session,
-            user_tg_id=callback.from_user.id,
-            i18n=i18n,
-            deafult_tz=deafult_tz,
-        ),
-    )
-    await callback.answer()
+# @in_systeam_router.callback_query(
+#     F.data == "week_schedule",
+#     StateFilter(default_state),
+# )
+# async def process_mounth_schedule_press(
+#     callback: CallbackQuery,
+#     session: AsyncSession,
+#     i18n: dict[str, dict[str, str]],
+#     deafult_tz: ZoneInfo,
+# ):
+#     await callback.message.edit_text(
+#         text=i18n["lexicon"]["week_schedule"],
+#         reply_markup=await create_week_shudle(
+#             session=session,
+#             user_tg_id=callback.from_user.id,
+#             i18n=i18n,
+#             deafult_tz=deafult_tz,
+#         ),
+#     )
+#     await callback.answer()
