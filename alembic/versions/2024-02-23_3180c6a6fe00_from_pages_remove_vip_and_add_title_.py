27c2faa2c65b39deed7ddb47c05fd30af055cc4b
Kuziil <Sikuz70@gmail.com>
2024-02-23 23:05:45 +0700

    Проблема с миграцией из-за type

--- alembic/versions/2024-02-23_3180c6a6fe00_from_pages_remove_vip_and_add_title_.py
+++ alembic/versions/2024-02-23_3180c6a6fe00_from_pages_remove_vip_and_add_title_.py
@@ -1,8 +1,8 @@
-"""from pages remove vip and add title, type, subscription_type, subscription_type
+"""from pages remove vip and add title, type_in_agency, subscription_type, subscription_type
 
-Revision ID: 7b2a44ea56cc
+Revision ID: 3180c6a6fe00
 Revises: 81aa10435585
-Create Date: 2024-02-23 22:46:51.973089
+Create Date: 2024-02-23 23:04:38.869240
 
 """
 
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "7b2a44ea56cc"
+revision = "3180c6a6fe00"
 down_revision = "81aa10435585"
 branch_labels = None
 depends_on = None
@@ -18,9 +18,10 @@ depends_on = None
 
 def upgrade():
     op.add_column("pages", sa.Column("title", sa.TEXT(), nullable=False))
-    op.add_column("pages", sa.Column("type", sa.TEXT(), nullable=False))
+    op.add_column("pages", sa.Column("type_in_agency", sa.TEXT(), nullable=False))
     op.add_column("pages", sa.Column("subscription_type", sa.TEXT(), nullable=False))
     op.add_column("pages", sa.Column("platform", sa.TEXT(), nullable=False))
+    op.add_column("pages", sa.Column("type", sa.TEXT(), nullable=False))
     op.drop_column("pages", "vip")
 
 
@@ -28,7 +29,8 @@ def downgrade():
     op.add_column(
         "pages", sa.Column("vip", sa.BOOLEAN(), autoincrement=False, nullable=False)
     )
+    op.drop_column("pages", "type")
     op.drop_column("pages", "platform")
     op.drop_column("pages", "subscription_type")
-    op.drop_column("pages", "type")
+    op.drop_column("pages", "type_in_agency")
     op.drop_column("pages", "title")
