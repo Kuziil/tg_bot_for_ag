08519f887d9446037094cc76782a6c16c50faea7
Kuziil <Sikuz70@gmail.com>
2024-02-27 23:45:04 +0700

    test_123

--- db/requests/with_page.py
+++ db/requests/with_page.py
@@ -1,8 +1,8 @@
 import datetime as dt
 
-from sqlalchemy import select, or_
+from sqlalchemy import select, or_, extract
 from sqlalchemy.engine import Result
-from sqlalchemy.orm import selectinload, joinedload
+from sqlalchemy.orm import selectinload, joinedload, contains_eager
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import PagesORM, PagesIntervalsORM, UsersORM, TgsORM, ShiftsORM
@@ -39,13 +39,47 @@ async def get_pages_with_inter_users_tgs_shifts_by_user_tg_id(
 
 async def test_123(
     session: AsyncSession,
+    user_tg_id: int,
 ):
-    result: Result = await session.execute(
-        select(PagesIntervalsORM)
-        .join(ShiftsORM, ShiftsORM.page_interval_id == PagesIntervalsORM.id)
+    month = dt.date.today().month
+
+    result = await session.execute(
+        select(PagesORM)
+        .join(PagesORM.intervals_details)
+        .join(PagesIntervalsORM.user)
+        .join(UsersORM.tgs)
+        .outerjoin(PagesIntervalsORM.shifts)
+        .where(
+            (extract("month", ShiftsORM.date_shift) == 1)
+            | (ShiftsORM.date_shift == None),
+        )
         .options(
-            selectinload(PagesIntervalsORM.shifts),
+            joinedload(PagesORM.model),
+            contains_eager(PagesORM.intervals_details).options(
+                joinedload(PagesIntervalsORM.interval),
+                joinedload(PagesIntervalsORM.user).selectinload(UsersORM.tgs),
+                contains_eager(PagesIntervalsORM.shifts),
+            ),
         )
     )
-    pages_intervals: list[PagesIntervalsORM] = result.scalars().all()
+    pages_intervals: list[PagesIntervalsORM] = result.scalars().unique().all()
     return pages_intervals
+
+
+# async def test_123(
+#     session: AsyncSession,
+# ):
+#     month = dt.date.today().month
+
+#     result = await session.execute(
+#         select(PagesIntervalsORM)
+#         .outerjoin(PagesIntervalsORM.shifts)
+#         .where(
+#             (extract("month", ShiftsORM.date_shift) == 1)
+#             | (ShiftsORM.date_shift == None)
+#         )
+#         .options(contains_eager(PagesIntervalsORM.shifts))
+#         .filter(PagesIntervalsORM.user_id < 5)  # добавили фильтр по user_id
+#     )
+#     pages_intervals: list[PagesIntervalsORM] = result.scalars().unique().all()
+#     return pages_intervals
