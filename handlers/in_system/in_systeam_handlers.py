e9218c159fa110e659c511b5c9cda34fc8b78553
Kuziil <Sikuz70@gmail.com>
2024-03-05 21:33:43 +0700

    удалил старые расписания

--- handlers/in_system/in_systeam_handlers.py
+++ handlers/in_system/in_systeam_handlers.py
@@ -7,9 +7,7 @@ from aiogram.fsm.state import default_state
 from aiogram.filters import or_f
 
 from lexicon.lexicon_ru import LEXICON_RU
-from keyboards.schedule.month.kb_month_schedule import create_schedule
 from keyboards.kb_single_line_vertically import create_menu_keyboard
-from handlers.in_system.schedules.month_handlers import schedule_router
 from handlers.in_system.schedules.month_v2_handlers import month_v2_router
 from keyboards.schedule.month_v2.bilder import create_month_shudle_v2
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -18,7 +16,6 @@ from callback_factories.back import BackCallbackData
 
 in_systeam_router = Router()
 
-in_systeam_router.include_router(schedule_router)
 in_systeam_router.include_router(month_v2_router)
 
 
@@ -45,21 +42,6 @@ async def process_schedule_press(
             "mounth_schedule",
             "week_schedule",
         ),
-    )
-    await callback.answer()
-
-
-@in_systeam_router.callback_query(
-    F.data == "mounth_schedule",
-    StateFilter(default_state),
-)
-async def process_mounth_schedule_press(
-    callback: CallbackQuery,
-    i18n: dict[str, dict[str, str]],
-):
-    await callback.message.edit_text(
-        text=i18n["lexicon"]["schedule"],
-        reply_markup=create_schedule(),
     )
     await callback.answer()
 
