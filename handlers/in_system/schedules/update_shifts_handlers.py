aaf44dda6847813e68570a77de3de09d9971a8ee
Kuziil <Sikuz70@gmail.com>
2024-03-09 15:23:36 +0700

    fix duplicate

--- handlers/in_system/schedules/update_shifts_handlers.py
+++ handlers/in_system/schedules/update_shifts_handlers.py
@@ -16,6 +16,30 @@ from keyboards.schedule.month_v2.classes_callback_data import (
 update_shifts_router = Router()
 
 
+async def update_shifts(session: AsyncSession, i18n: dict[dict[str, str]], callback, callback_data,
+                        default_tz: ZoneInfo,
+                        st_shifts: list[dict[str, str]] | None = None,
+                        state: FSMContext | None = None, ):
+    if st_shifts is None:
+        st: dict[str, list[dict[str, str]]] = await state.get_data()
+        st_shifts: list[dict[str, str]] = st["shifts"]
+
+    markup, st_shifts = await create_month_schedule_v2(
+        user_tg_id=callback.from_user.id,
+        session=session,
+        i18n=i18n,
+        default_tz=default_tz,
+        current_month=callback_data.month,
+        current_year=callback_data.year,
+        current_day=callback_data.day,
+        current_page_id=callback_data.page_id,
+        current_interval_id=callback_data.interval_id,
+        current_lineup=callback_data.lineup,
+        st_shifts=st_shifts,
+    )
+    return markup, st_shifts
+
+
 @update_shifts_router.callback_query(
     StateFilter(FSMSetShifts),
     MonthScheduleCallbackData.filter(F.day > 0),
@@ -30,17 +54,12 @@ async def process_days_press(
         state: FSMContext,
         st_shifts: list[dict[str, str]],
 ):
-    markup, st_shifts = await create_month_schedule_v2(
-        user_tg_id=callback.from_user.id,
+    markup, st_shifts = await update_shifts(
         session=session,
         i18n=i18n,
+        callback=callback,
+        callback_data=callback_data,
         default_tz=default_tz,
-        current_month=callback_data.month,
-        current_year=callback_data.year,
-        current_day=callback_data.day,
-        current_page_id=callback_data.page_id,
-        current_interval_id=callback_data.interval_id,
-        current_lineup=callback_data.lineup,
         st_shifts=st_shifts,
     )
     await callback.message.edit_text(
@@ -62,20 +81,13 @@ async def process_busy_days_press(
         i18n: dict[dict[str, str]],
         state: FSMContext,
 ):
-    st: dict[str, str] = await state.get_data()
-    st_shifts: list[dict[str, str]] = st["shifts"]
-    markup, st_shifts = await create_month_schedule_v2(
-        user_tg_id=callback.from_user.id,
+    markup, st_shifts = await update_shifts(
         session=session,
         i18n=i18n,
+        callback=callback,
+        callback_data=callback_data,
         default_tz=default_tz,
-        current_month=callback_data.month,
-        current_year=callback_data.year,
-        current_day=callback_data.day,
-        current_page_id=callback_data.page_id,
-        current_interval_id=callback_data.interval_id,
-        current_lineup=callback_data.lineup,
-        st_shifts=st_shifts,
+        state=state,
     )
     await callback.message.edit_text(
         text="3",
@@ -96,19 +108,14 @@ async def process_apply_in_st(
         i18n: dict[dict[str, str]],
         state: FSMContext,
 ):
-    st: dict[str, str] = await state.get_data()
+    st: dict[str, list[dict[str, str]]] = await state.get_data()
     st_shifts: list[dict[str, str]] = st["shifts"]
-    markup, st_shifts_f = await create_month_schedule_v2(
-        user_tg_id=callback.from_user.id,
+    markup, st_shifts_f = await update_shifts(
         session=session,
         i18n=i18n,
+        callback=callback,
+        callback_data=callback_data,
         default_tz=default_tz,
-        current_month=callback_data.month,
-        current_year=callback_data.year,
-        current_day=callback_data.day,
-        current_page_id=callback_data.page_id,
-        current_interval_id=callback_data.interval_id,
-        current_lineup=callback_data.lineup,
         st_shifts=st_shifts,
     )
     if st_shifts_f == st_shifts:
@@ -183,20 +190,13 @@ async def process_not_day_press_in_st(
         i18n: dict[dict[str, str]],
         state: FSMContext,
 ):
-    st: dict[str, str] = await state.get_data()
-    st_shifts: list[dict[str, str]] = st["shifts"]
-    markup, st_shifts = await create_month_schedule_v2(
-        user_tg_id=callback.from_user.id,
+    markup, st_shifts = await update_shifts(
         session=session,
         i18n=i18n,
+        callback=callback,
+        callback_data=callback_data,
         default_tz=default_tz,
-        current_month=callback_data.month,
-        current_year=callback_data.year,
-        current_day=callback_data.day,
-        current_page_id=callback_data.page_id,
-        current_interval_id=callback_data.interval_id,
-        current_lineup=callback_data.lineup,
-        st_shifts=st_shifts,
+        state=state,
     )
     await callback.message.edit_text(
         text="3",
